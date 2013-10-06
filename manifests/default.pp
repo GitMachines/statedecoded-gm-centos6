623816f92dde5189d8e3454d362a7ce0758c4392
Greg Elin <greg@fotonotes.net>
2013-10-06 14:41:36 -0400

    Add gmdoc links. Add /tmp/facts.yaml output

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,5 +1,17 @@
-notify {"Super simple LAMP":}
+notify {"@ Super simple LAMP":}
 
+# { "gmdoc": "https://gist.github.com/gregelin/6857377" }
 class { 'apache': }
 
+# { "gmdoc": "https://gist.github.com/gregelin/6857258" }
 # class { 'mysql::server': }
+
+/*
+ { "gmdoc": {
+ 	"title": "Capture basic report",
+   }
+ }
+*/
+file { "/tmp/facts.yaml":
+    content => inline_template("<%= scope.to_hash.reject { |k,v| !( k.is_a?(String) && v.is_a?(String) ) }.to_yaml %>"),
+}
