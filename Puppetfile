97d92fc4ede9c9591e08091e362396e89984c979
John Lancaster <john.k.lancaster@gmail.com>
2013-10-12 12:18:54 -0400

    added php module, trying to get git module to check out statedecoded master branch

--- Puppetfile
+++ Puppetfile
@@ -2,6 +2,7 @@ forge "http://forge.puppetlabs.com"
 
 mod "puppetlabs/apache"
 mod "puppetlabs/firewall"
-# mod "puppetlabs/mysql"
-# mod "thias/php"
-
+mod "puppetlabs/mysql"
+mod "thias/php"
+mod "puppetlabs/git"
+mod "puppetlabs/vcsrepo"
--- manifests/default.pp
+++ manifests/default.pp
@@ -3,7 +3,6 @@ notify {"@ Super simple LAMP":}
 # { "gmdoc": "https://gist.github.com/gregelin/6857377" }
 class { 'apache': }
 
-
 # { "gmdoc": "https://gist.github.com/gregelin/6858369"}
 resources { "firewall":
   # remove existing firewall rules
@@ -18,7 +17,31 @@ class { 'firewall': }
 
 
 # { "gmdoc": "https://gist.github.com/gregelin/6857258" }
-# class { 'mysql::server': }
+class { 'mysql::server': }
+
+php::ini { '/etc/php.ini':
+  display_errors => 'On',
+  memory_limit   => '256M',
+}
+
+php::ini { '/etc/httpd/conf/php.ini':
+  mail_add_x_header => 'Off',
+  # For the parent directory
+  require => Package['httpd'],
+}
+
+class { 'php::mod_php5': inifile => '/etc/httpd/conf/php.ini' }
+
+class { 'git': }
+
+vcsrepo { '/var/www/html/statedecoded/':
+    ensure   => latest,
+    user     => $user,
+    provider => git,
+    require  => Package["git"],
+    source   => "git@github.com:statedecoded/statedecoded.git",
+    revision => 'master',
+  }
 
 /*
  { "gmdoc": {
