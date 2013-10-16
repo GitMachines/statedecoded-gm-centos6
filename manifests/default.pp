ab752f11b186fceff51122df6edf988bbe217247
Ikjae Park <chunhamujuk@gmail.com>
2013-10-15 21:37:17 -0400

    Updated php module and mysql module in php

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,13 +5,10 @@ class{ 'epel': }
 # { "gmdoc": "https://gist.github.com/gregelin/6857377" }
 class { 'apache':  }
 
-##apache::mod { 'rewrite': }
 apache::vhost { 'statedecoded.dev':
-  docroot       => '/var/www/html/statedecoded',
+  docroot       => '/var/www/html/statedecoded/htdocs/',
   port          => '80',
 }
-
-apache::mod { 'php': }
 
 # { "gmdoc": "https://gist.github.com/gregelin/6858369"}
 resources { "firewall":
@@ -40,37 +37,17 @@ file { "/tmp/facts.yaml":
     content => inline_template("<%= scope.to_hash.reject { |k,v| !( k.is_a?(String) && v.is_a?(String) ) }.to_yaml %>"),
 }
 
-#class { 'php': 
-#  service => 'apache',
-#  require => Class['apache'],
-#}
+php::ini { '/etc/php.ini':
+  display_errors => 'On',
+  memory_limit   => '256M',
+}
 
-php::module { 'mysql': }
-php::module { 'cli': }
-php::module { 'curl': }
-php::module { 'intl': }
-#php::module { 'mcrypt': }
-#php::module { 'mysqlnd': }
-php::module { 'tidy': }
-php::module { 'devel': }
+php::ini { '/etc/httpd/conf/php.ini':
+  mail_add_x_header => 'Off',
+  # For the parent directory
+  require => Class['apache'],
+}
 
-#class { 'php::devel':
-#  require => Class['php'],
-#}
+class { 'php::mod_php5': inifile => '/etc/httpd/conf/php.ini' }
 
-#class { 'php::pear':
-#  require => Class['php'],
-#}
-
-# Installation of solr
-# install open-jdk 1.6
-#package{"java-1.6.0-openjdk-devel":
-#  ensure=>latest
-#}
-
-# install tomcat & co.
-#package {["tomcat6", "tomcat6-webapps", "tomcat6-admin-webapps"]:
-#  ensure =>"present"
-#}
-
-
+php::module { [ 'mysql' ]: }
