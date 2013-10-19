45782ef0429f642853e064256f94c307a99a12eb
Greg Elin <greg@fotonotes.net>
2013-10-19 14:52:19 -0400

    Add comment for facts.yaml

--- manifests/default.pp
+++ manifests/default.pp
@@ -89,6 +89,7 @@ mysql::db { 'statedecoded':
 	grant		=> 'ALL'
 }
 
+# Write configuration facts to file /tmp/facts.yaml
 file { "/tmp/facts.yaml":
     content => inline_template("<%= scope.to_hash.reject { |k,v| !( k.is_a?(String) && v.is_a?(String) ) }.to_yaml %>"),
 }
