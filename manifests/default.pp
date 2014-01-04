afba1f5389539cba08447cea8e79f73e3bb3bb38
Greg Elin <greg@fotonotes.net>
2014-01-04 08:01:51 -0500

    Use Statededecoded release 0.81

--- manifests/default.pp
+++ manifests/default.pp
@@ -61,9 +61,10 @@ exec { 'unzip-laws':
 }
 	
 exec { 'get-statedecoded':
-	command		=> '/usr/bin/wget -S -O - https://github.com/statedecoded/statedecoded/archive/master.tar.gz| /bin/tar zx --strip 1',
-	cwd		=> '/var/www/html/statedecoded',
-	require         => Class['apache'],
+  command   => '/usr/bin/wget -S -O - https://github.com/statedecoded/statedecoded/archive/v0.81.tar.gz| /bin/tar zx --strip 1',
+  # command   => '/usr/bin/wget -S -O - https://github.com/statedecoded/statedecoded/archive/master.tar.gz| /bin/tar zx --strip 1',
+  cwd   => '/var/www/html/statedecoded',
+  require         => Class['apache'],
 }
 
 #exec { 'unzip-statedecoded':
