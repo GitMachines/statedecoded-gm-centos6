a63e614dcf40a60e25934b392aff06c71d210c8c
Greg Elin <greg@fotonotes.net>
2014-01-04 14:51:06 -0500

    Add reports directory stub and fix paths for oscap test script

--- audit/resources/scripts/oscap-rhel6-test2.sh
+++ audit/resources/scripts/oscap-rhel6-test2.sh
@@ -2,7 +2,7 @@
 
 oscap xccdf eval --profile test \
 	--results /vagrant/audit/reports/ssg-results-test2.xml \
-	--report /vagrant/ssg-results-test2.html \
-	--cpe /vagrant/resources/ssg/content/ssg-rhel6-cpe-dictionary.xml \
-	/vagrant/resources/ssg/content/ssg-rhel6-test2-xccdf.xml
+	--report /vagrant/audit/reports/ssg-results-test2.html \
+	--cpe /vagrant/audit/resources/ssg/content/ssg-rhel6-cpe-dictionary.xml \
+	/vagrant/audit/resources/ssg/content/ssg-rhel6-test2-xccdf.xml
 
