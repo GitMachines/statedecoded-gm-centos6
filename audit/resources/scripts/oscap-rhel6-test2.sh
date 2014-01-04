7d30f8bb35ca33d0e805f9c407b69a9b6b5bc508
Greg Elin <greg@fotonotes.net>
2014-01-04 08:03:16 -0500

    Fix oscap-rhel6-test2.sh and add audit home.html page stub

--- audit/resources/scripts/oscap-rhel6-test2.sh
+++ audit/resources/scripts/oscap-rhel6-test2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 oscap xccdf eval --profile test \
-	--results /vagrant/`hostname`-ssg-test2-results.xml \
-	--report /vagrant/`hostname`-ssg-test2-results.html \
-	--cpe /vagrant/ssg/content/ssg-rhel6-cpe-dictionary.xml \
-	/vagrant/ssg/content/ssg-rhel6-test2-xccdf.xml
+	--results /vagrant/audit/reports/ssg-results-test2.xml \
+	--report /vagrant/ssg-results-test2.html \
+	--cpe /vagrant/resources/ssg/content/ssg-rhel6-cpe-dictionary.xml \
+	/vagrant/resources/ssg/content/ssg-rhel6-test2-xccdf.xml
 
