782a00899a532fa352985457cae7d75bb978efdd
Greg Elin <greg@fotonotes.net>
2014-01-26 12:28:28 -0500

    Close #32. Add bash script to automate import of sample laws via curl post to admin form.

---
+++ misc/scripts/import-sample-laws.sh
@@ -0,0 +1,2 @@
+# shell script to import sample laws
+time curl --data 'action=parse&edition_option=new&new_edition_name=2014-sample&new_edition_slug=2014-sample&make_current=1' 'http://localhost:8080/admin/?page=parse&noframe=1'
